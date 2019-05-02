1e53d911afa889292dec2969394a14b678a8cebb
Laura <la.vermiglio@gmail.com>
2019-05-02 10:22:57 +0200

    Initial Commit

---
+++ SuperCalc/src/it/rgi/supercalc/SuperCalc.java
@@ -0,0 +1,31 @@
+/**
+ * 
+ */
+package it.rgi.supercalc;
+
+/**
+ * @author academy1418r.01
+ *
+ */
+public class SuperCalc {
+	
+	private int ret;
+
+	/**
+	 * 
+	 */
+	public SuperCalc() {
+		// TODO Auto-generated constructor stub
+	}
+	
+	public int sum(int op1, int op2) {
+		ret=op1+op2;
+		return ret;
+	}
+	
+	public int sub(int op1, int op2) {
+		ret=op1-op2;
+		return ret;
+	}
+
+}
